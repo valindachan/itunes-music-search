1aa3a4ce9ffa37ad0506f4daf75d805bedf5d260
shaunhailey <ediblelysol@gmail.com>
2017-07-25 08:47:39 -0400

    Music player

--- main.js
+++ main.js
@@ -11,10 +11,12 @@
 // let search = document
 //   .querySelector("form.search-form")
 //   .addEventListener("submit", search)
+let previewUrls = []
+let previewElements = []
 
 function search() {
-  let url = "https://itunes.apple.com/search?term="
-  let searchValue = document.querySelector("#searchBox").value
+  let url = 'https://itunes.apple.com/search?term='
+  let searchValue = document.querySelector('#searchBox').value
 
   url = `${url}${searchValue}&limit=15`
   // console.log(url)
@@ -22,47 +24,59 @@ function search() {
   let artistName
   let trackName
   let artworkUrl100
-  let previewUrl
-  let collectionName // Album name
+  let collectionName
+  // Album name
   // console.log(data)
 
-  let results = document.querySelector(".results")
+  let results = document.querySelector('.results')
 
   // Select and store the information we want to work with
   fetch(url).then(response => response.json()).then(data => {
+    console.log(data)
     for (var i = 0; i < data.results.length; i++) {
       artistName = data.results[i].artistName
       trackName = data.results[i].trackName
       artworkUrl100 = data.results[i].artworkUrl100
       previewUrl = data.results[i].previewUrl
-      anchor = "#"
+      previewUrls.push(previewUrl)
+      anchor = ''
+      anchor.onclick = 'playOnPlayer()'
+      console.log(anchor)
       collectionName = data.results[i].collectionName
 
-      let artistNameText = document.createElement("h2")
-      let trackNameText = document.createElement("h3")
-      let artworkUrl100Img = document.createElement("img")
-      let anchorContainer = document.createElement("a")
-      let collectionNameText = document.createElement("h4")
-      let resultContainer = document.createElement("div")
+      let artistNameText = document.createElement('h2')
+      let trackNameText = document.createElement('h3')
+      let artworkUrl100Img = document.createElement('img')
+      let anchorContainer = document.createElement('a')
+      let collectionNameText = document.createElement('h4')
+      let resultContainer = document.createElement('div')
 
       artworkUrl100Img.src = artworkUrl100
       trackNameText.textContent = trackName
       artistNameText.textContent = artistName
       collectionNameText.textContent = collectionName
-      anchorContainer.href = anchor
 
       anchorContainer.appendChild(artworkUrl100Img)
       anchorContainer.appendChild(trackNameText)
-      anchorContainer.appendChild(collectionNameText)
       anchorContainer.appendChild(artistNameText)
       resultContainer.appendChild(anchorContainer)
+      resultContainer.id = i
       results.appendChild(resultContainer)
-
-      console.log(results)
+      resultContainer.addEventListener('click', playOnPlayer)
     }
+    console.log(previewElements)
 
-    url = "https://itunes.apple.com/search?term="
+    url = 'https://itunes.apple.com/search?term='
   })
+}
+
+function playOnPlayer(event) {
+  let player = document.querySelector('audio')
+  let toBePlayed = previewUrls[event.target.id]
+  player.src = toBePlayed
+
+  console.log(toBePlayed)
+  // anchor.setAttribute(src, previewUrl)
 }
 
 // If 'Enter' key is hit, start searching input typed in
